f79cca0fe791c003d8de18d6339cc40c78720376
Lukasz Dabrowski <ldabrowski@advaoptical.com>
2018-10-01 16:14:10 +0200

    move module to subpackage to isolate it from other backports, e.g. backports.os

---
+++ backports/shutil_which/__init__.py
@@ -0,0 +1,10 @@
+"""Backport of shutil.which from Python 3.5
+
+The function is included unmodified from Python stdlib 3.5.1,
+and is (C) Python
+"""
+
+from .shutil_which import which
+
+
+__all__ = ['which']
